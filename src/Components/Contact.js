34ccd8feba98192abfafab2773145b7873b40164
Akhilesh Sharma <58417806+akhileshsharma08@users.noreply.github.com>
2023-04-26 23:02:57 +0530

    contact page added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,40 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      contact page
+      <div className="py-20">
+        <div className="container flex md:flex-row flex-col justify-around items-center py-4">
+          <div className="tagline md:w-1/2 w-full bg-white p-4">
+            <h1 className='md:text-6xl text-4xl font-bold' >Need additional information?</h1>
+            <h2 className='text-2xl text-gray-600 my-4'>A multifaceted professional skilled in multiple fields of research, development as well as a learning specialist. Over 15 years of experience.</h2>
+            <div className="box text-gray-900 font-bold text-2xl my-2">
+              <h2 className='hover:text-red-600 my-2 cursor'>📞(123) 456-7869</h2>
+              <h2 className='hover:text-red-600 my-2 cursor'> 📧carrental@carmail.com</h2>
+              <h2 className='hover:text-red-600 my-2 cursor'> 📍 Belgrade, Serbia</h2>
+            </div>
+
+          </div>
+          <div className="contact-form md:w-1/2 w-full bg-white p-4 shadow-lg mb-4 mt-8">
+            <div className="  bg-white flex flex-col md:ml-auto w-full md:py-4 mt-8 md:mt-0 ">
+              {/* <h2 className="text-gray-900 text-lg mb-1 font-medium title-font my-4">Contact</h2> */}
+              {/* <p className="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p> */}
+              <div className="relative mb-4">
+                <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                <input type="text" id="name" name="name" className="w-full bg-white  border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <div className="relative mb-4">
+                <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                <input type="email" id="email" name="email" className="w-full bg-white  border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+              </div>
+              <div className="relative mb-4">
+                <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+                <textarea id="message" name="message" className="w-full bg-white  border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+              </div>
+              <button className="text-white bg-red-600 border-0 py-2 px-6 focus:outline-none hover:bg-red-500  text-lg">📤 Send</button>
+              <p className="text-xs text-white mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -18,23 +18,23 @@ const Faq = () => {
                                 on Our Website: Answers to Common Concerns and Inquiries.
                             </p>
                         </div>
-                        <div className="container mx-auto">
-                            <div className="container">
+                        <div className="container mx-auto ">
+                            <div className="container mx-auto">
                                 <Accordion>
                                 <Accordion.Item eventKey="0">
-                                    <Accordion.Header className="text-light bg-danger">1.What is special about comparing rental car deals?</Accordion.Header>
+                                    <Accordion.Header className="text-white myaccordianHeading bg-danger">1.What is special about comparing rental car deals?</Accordion.Header>
                                     <Accordion.Body>
                                         Comparing rental car deals is important as it helps find the best deal that fits your budget and requirements, ensuring you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or better car models. You can find car rental deals by researching online and comparing prices from different rental companies.
                                     </Accordion.Body>
                                 </Accordion.Item>
                                 <Accordion.Item eventKey="1">
-                                    <Accordion.Header className="text-light bg-danger">2. How do I find the car rental deals?</Accordion.Header>
+                                    <Accordion.Header className="text-white myaccordianHeading bg-danger">2. How do I find the car rental deals?</Accordion.Header>
                                     <Accordion.Body>
                                         You can find car rental deals by researching online and comparing prices from different rental companies. Websites such as Expedia, Kayak, and Travelocity allow you to compare prices and view available rental options. It is also recommended to sign up for email newsletters and follow rental car companies on social media to be informed of any special deals or promotions.
                                     </Accordion.Body>
                                 </Accordion.Item>
                                 <Accordion.Item eventKey="2">
-                                    <Accordion.Header className="text-light bg-danger">3. How do I find such low rental car prices?</Accordion.Header>
+                                    <Accordion.Header className="text-white myaccordianHeading bg-danger">3. How do I find such low rental car prices?</Accordion.Header>
                                     <Accordion.Body className="text-secondary">
                                     Book in advance: Booking your rental car ahead of time can often result in lower prices. Compare prices from multiple companies: Use websites like Kayak, Expedia, or Travelocity to compare prices from multiple rental car companies. Look for discount codes and coupons: Search for discount codes and coupons that you can use to lower the rental price. Renting from an off-airport location can sometimes result in lower prices.
                                     </Accordion.Body>
